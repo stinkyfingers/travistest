30a0759ff382f7d60a84f233aa94600ff2c462fc
stinkyfingers <john_shenk@hotmail.com>
2021-06-08 11:21:52 -0500

    revise

--- cli_tests/add.sh
+++ cli_tests/add.sh
@@ -5,7 +5,7 @@
   [ "$result" -eq 4 ]
 }
 
-@test "addition using dc" {
-  result="$(echo 2 2+p | dc)"
-  [ "$result" -eq 4 ]
-}
+# @test "addition using dc" {
+#   result="$(echo 2 2+p | dc)"
+#   [ "$result" -eq 4 ]
+# }
