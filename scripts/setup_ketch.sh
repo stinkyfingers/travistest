fe4b3be7b1b653414d6baa69c34d2d3f4c5794b2
stinkyfingers <john_shenk@hotmail.com>
2021-06-08 14:37:16 -0500

    use k8 version 1.21

--- scripts/setup_ketch.sh
+++ scripts/setup_ketch.sh
@@ -2,24 +2,27 @@
 
 TRAVIS_COMMIT=$1
 
+# ketch
 export KUBECONFIG="$(kind get kubeconfig-path)"
 kubectl cluster-info
 # make install
 # make ketch
-sudo curl -s https://raw.githubusercontent.com/shipa-corp/ketch/main/install.sh | sudo bash
-
+sudo curl -s https://raw.githubusercontent.com/shipa-corp/ketch/main/install.sh | sudo bash # TODO
 ketch -v
 
 # cert-manager
 kubectl apply -f https://github.com/jetstack/cert-manager/releases/download/v1.3.1/cert-manager.yaml
+
 # istio
 curl -Ls https://istio.io/downloadIstio | ISTIO_VERSION=1.9.0 sh -
 export PATH=$PWD/istio-1.9.0/bin:$PATH
 istioctl install --set profile=demo -y
+
 # wait for containers
 kubectl wait --for=condition=Ready=true pod -n cert-manager --all
 kubectl wait --for=condition=Ready=true pod -n istio-system --all
 kubectl get pods -A
+
 # deploy
 # make deploy IMG=shipasoftware/ketch:$TRAVIS_COMMIT
-kubectl apply -f https://github.com/shipa-corp/ketch/releases/download/v0.2.1/ketch-controller.yaml
+kubectl apply -f https://github.com/shipa-corp/ketch/releases/download/v0.2.1/ketch-controller.yaml # TODO
--- scripts/setup_kind.sh
+++ scripts/setup_kind.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+KUBERNETES_IMAGE=kindest/node:v1.21.1
+
 # Download and install kubectl (optional)
 curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.12.0/bin/linux/amd64/kubectl && chmod +x kubectl && sudo mv kubectl /usr/local/bin/
 # Download and install kind using Go toolchain
@@ -7,9 +9,9 @@ curl -Lo kubectl https://storage.googleapis.com/kubernetes-release/release/v1.12
 # Download and install kind using cURL
 curl -Lo kind https://github.com/kubernetes-sigs/kind/releases/download/0.0.1/kind-linux-amd64 && chmod +x kind && sudo mv kind /usr/local/bin/
 # Create a new Kubernetes cluster using kind
-kind create cluster
+kind create cluster --image=$KUBERNETES_IMAGE
 # Set KUBECONFIG environment variable
-export KUBECONFIG="$(kind get kubeconfig-path)"
+# export KUBECONFIG="$(kind get kubeconfig-path)"
 
 
 # Kubebuiler & Kustomize
