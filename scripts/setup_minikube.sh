2c6c9bbbab4f4862a4d6c5c57cb53660e412e43b
stinkyfingers <john_shenk@hotmail.com>
2021-06-08 14:55:05 -0500

    enable minikube

--- scripts/setup_minikube.sh
+++ scripts/setup_minikube.sh
@@ -13,24 +13,24 @@ mkdir -p $HOME/.kube $HOME/.minikube
 touch "$KUBECONFIG"
 sudo minikube start --profile=minikube --vm-driver=none --kubernetes-version=v1.20.1
 minikube update-context --profile=minikube
-"sudo chown -R travis: /home/travis/.minikube/"
+sudo chown -R travis: /home/travis/.minikube/
 eval "$(minikube docker-env --profile=minikube)" && export DOCKER_CLI='docker'
 # TODO - rm and rely on push from docker repo, make type == PR
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
-docker build -t shipasoftware/ketch:$TRAVIS_COMMIT .
-docker push shipasoftware/ketch:$TRAVIS_COMMIT
-
-# Kubebuiler & Kustomize
-make install-kubebuilder KUBEBUILDER_INSTALL_DIR=/tmp/kubebuilder
-make install-kustomize KUSTOMIZE_INSTALL_DIR=/tmp
-export PATH=$PATH:/tmp
-
-# Helm
-curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
-sudo apt-get install apt-transport-https --yes
-echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
-sudo apt-get update
-sudo apt-get install helm
-
-# BATS
-sudo apt-get update -yq && sudo apt-get install bats -y
+# echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USERNAME" --password-stdin
+# docker build -t shipasoftware/ketch:$TRAVIS_COMMIT .
+# docker push shipasoftware/ketch:$TRAVIS_COMMIT
+#
+# # Kubebuiler & Kustomize
+# make install-kubebuilder KUBEBUILDER_INSTALL_DIR=/tmp/kubebuilder
+# make install-kustomize KUSTOMIZE_INSTALL_DIR=/tmp
+# export PATH=$PATH:/tmp
+#
+# # Helm
+# curl https://baltocdn.com/helm/signing.asc | sudo apt-key add -
+# sudo apt-get install apt-transport-https --yes
+# echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+# sudo apt-get update
+# sudo apt-get install helm
+#
+# # BATS
+# sudo apt-get update -yq && sudo apt-get install bats -y
