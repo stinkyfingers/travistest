9ee2cfd1a28c20f823e8cbe7d9db58e6c9f5d9e5
stinkyfingers <john_shenk@hotmail.com>
2021-06-09 10:07:40 -0500

    sudo apt

--- scripts/install_minikube.sh
+++ scripts/install_minikube.sh
@@ -7,7 +7,7 @@ export MINIKUBE_HOME=$HOME
 export KUBECONFIG=$HOME/.kube/config
 
 # Minikube
-sudo apt-get update -yq && apt-get -qq -y install conntrack
+sudo apt-get update -yq && sudo apt-get -qq -y install conntrack
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/v1.16.0/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 mkdir -p $HOME/.kube $HOME/.minikube
 touch "$KUBECONFIG"
