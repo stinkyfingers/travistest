1cabd4c5c69c8022f505ca8aafd6d9082c6c2cdb
stinkyfingers <john_shenk@hotmail.com>
2021-06-09 17:32:28 -0500

    This is a combination of 6 commits.
    um
    
    um
    
    quote strings
    
    no url
    
    make things more readable
    
     This is a combination of 3 commits.
    
    sample-go-app
    
    noop 1
    
    noop 2

--- cli_tests/app.sh
+++ cli_tests/app.sh
@@ -1,19 +1,19 @@
 #!/usr/bin/env bats
 
 # To run locally:
-# export KETCH_COMMAND=<location of ketch binary>
+# export KETCH_EXECUTABLE_PATH=<location of ketch binary>
 # assure you have a kubernetes cluster running w/ traefik, cert manager, etc. (see ketch getting started docs)
 # ./app.sh
 
 setup() {
-  if [[ -z "${KETCH_COMMAND}" ]]; then
+  if [[ -z "${KETCH_EXECUTABLE_PATH}" ]]; then
     KETCH=$(pwd)/ketch/bin/ketch
   else
-    KETCH="${KETCH_COMMAND}"
+    KETCH="${KETCH_EXECUTABLE_PATH}"
   fi
   INGRESS=$(kubectl get svc traefik -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
   FRAMEWORK="myframework"
-  APP_IMAGE="docker.io/shipasoftware/bulletinboard:1.0"
+  APP_IMAGE="gcr.io/shipa-ci/sample-go-app:latest"
 }
 
 
@@ -25,13 +25,14 @@ setup() {
 }
 
 @test "framework add" {
-  echo "ADD" $KETCH framework add --ingress-service-endpoint $INGRESS --ingress-type traefik
-  result=$($KETCH framework add --ingress-service-endpoint $INGRESS --ingress-type traefik)
+  echo "ADD" $KETCH framework add "$FRAMEWORK" --ingress-service-endpoint "$INGRESS" --ingress-type "traefik"
+  result=$($KETCH framework add "$FRAMEWORK" --ingress-service-endpoint "$INGRESS" --ingress-type "traefik")
   [[ $result =~ "Successfully added!" ]]
 }
 
 @test "framework list" {
   result=$($KETCH framework list)
+  echo "RESULT" $result
   headerRegex="NAME[ \t]+STATUS[ \t]+NAMESPACE[ \t]+INGRESS TYPE[ \t]+INGRESS CLASS NAME[ \t]+CLUSTER ISSUER[ \t]+APPS"
   dataRegex="myframework[ \t]+ketch-myframework[ \t]+traefik[ \t]+traefik"
   [[ $result =~ $headerRegex ]]
@@ -39,25 +40,30 @@ setup() {
 }
 
 @test "app deploy" {
-  run $KETCH app deploy bulletinboard --framework $FRAMEWORK -i $APP_IMAGE
-  [ $status -eq 0 ]
+  run $KETCH app deploy bulletinboard --framework "$FRAMEWORK" -i "$APP_IMAGE"
+  [[ $status -eq 0 ]]
 }
 
 @test "app list" {
   result=$($KETCH app list)
   headerRegex="NAME[ \t]+FRAMEWORK[ \t]+STATE[ \t]+ADDRESSES[ \t]+BUILDER[ \t]+DESCRIPTION"
-  dataRegex="bulletinboard[ \t]+myframework[ \t]+(created|running)[ \t]+http://bulletinboard.$INGRESS.shipa.cloud"
+  dataRegex="bulletinboard[ \t]+myframework[ \t]+(created|running)"
   echo $result
   [[ $result =~ $headerRegex ]]
   [[ $result =~ $dataRegex ]]
 }
 
-@test "app remove" {
-  result=$($KETCH app remove bulletinboard)
-  [[ $result =~ "Successfully removed!" ]]
+# new
+@test "app info" {
+  result=$($KETCH app info bulletinboard)
 }
 
-@test "framework remove" {
-  result=$(echo ketch-$FRAMEWORK | $KETCH framework remove $FRAMEWORK)
-  [[ $result =~ "Framework successfully removed!" ]]
-}
+# @test "app remove" {
+#   result=$($KETCH app remove bulletinboard)
+#   [[ $result =~ "Successfully removed!" ]]
+# }
+#
+# @test "framework remove" {
+#   result=$(echo "ketch-$FRAMEWORK" | $KETCH framework remove "$FRAMEWORK")
+#   [[ $result =~ "Framework successfully removed!" ]]
+# }
